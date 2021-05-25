1fd7f39b384a7f87f1e16e61106ff214fbc4cc48
Jaimin Shimpi <jaiminshimpi02@gmail.com>
2021-05-25 15:35:15 +0530

    Initial Commit

---
+++ Scripts/Clock.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Clock : UnityEngine.MonoBehaviour
+{
+	const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+	[SerializeField]
+	Transform hoursPivot, minutesPivot, secondsPivot;
+
+	void Update()
+	{
+		TimeSpan time = DateTime.Now.TimeOfDay;
+		hoursPivot.localRotation =
+			Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+		minutesPivot.localRotation =
+			Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+		secondsPivot.localRotation =
+			Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+	}
+}
